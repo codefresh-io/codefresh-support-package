278bbee6c3dc1c4acb2add23df6215222508a9e8
Britton Riggs <britton.riggs@octopus.com>
2025-08-18 08:23:44 -0700

    Structuring Utilities file, adding some logic

--- src/utilities/utility.py
+++ src/utilities/utility.py
@@ -1,4 +1,5 @@
 from utilities.logger_config import setup_logger
+import shutil
 import tarfile
 import yaml
 import os
@@ -10,22 +11,27 @@ class Utility:
     def __init__(self) -> None:
         pass
 
-    def write_yaml_to_file(self, py_obj: object, filename: str):
+    def write_yaml(self, py_obj: object, filename: str):
         with open(f"{filename}.yaml", "w") as f:
             yaml.dump(py_obj, f, sort_keys=False)
         print("Written to file successfully")
 
-    def create_tar_gz(self, output_filename: str, source_dir: str):
-        if not os.path.isdir(source_dir):
-            raise NotADirectoryError(f"{source_dir} is not a valid directory")
-
-        with tarfile.open(output_filename, "w:gz") as tar:
-            tar.add(source_dir, arcname=os.path.basename(source_dir))
-
-    def prepare_package(self, dir_path: str):
+    def prepare_package(self, output_filename: str, dir_path: str):
         compressed_support_package = f"{dir_path}.tar.gz"
+        logger.info(f"{self.__class__.__name__} Preparing the support package")
 
-        logger.info(f"{self.__class__.__name__} Preparing the support package...")
+        if not os.path.isdir(dir_path):
+            raise NotADirectoryError(f"{dir_path} is not a valid directory")
 
-    def process_dat(self):
-        pass
+        try:
+            with tarfile.open(output_filename, "w:gz") as tar:
+                tar.add(dir_path, arcname=os.path.basename(dir_path))
+        except Exception as error:
+            logger.error(
+                f"{self.__class__.__name__} {error}\n\nPlease manually compress the directory {dir_path} and attach it to the support ticket."
+            )
+        else:
+            shutil.rmtree(dir_path)
+
+    def process_data(self, dir_path, k8s_resource):
+        logger.info(f"{self.__class__.__name__} Processing and saving data")
